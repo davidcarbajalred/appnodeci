f1e7696d47a8aa4f33ad4836be32b53fab76027e
Deivid <tucorreo@ejemplo.com>
2026-06-10 21:52:48 +0200

    add jest testing

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.status(200).json({
-        'message': 'Hola Mundo'
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hola Mundo",
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Servidor escuchando en el puerto ${port}`);
-})
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
+
+module.exports = app;
---
+++ app.test.js
@@ -0,0 +1,13 @@
+const app = require("./app");
+const supertest = require("supertest");
+const request = supertest(app);
+
+describe("/test endpoint", () => {
+    test("Debe devolver Hola Mundo", async () => {
+        const response = await request.get("/");
+        const body = JSON.parse(response.text);
+
+        expect(response.status).toBe(200);
+        expect(body.message).toBe("Hola Mundo");
+    });
+});
